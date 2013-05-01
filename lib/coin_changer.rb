7c86e523bb935ca75879bcfaaf6a91cb76b9b86c
Dave Kaplan <workalicious@gmail.com>
2013-04-30 20:23:11 -0500

    passed tests and added dave_kaplan

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -2,6 +2,11 @@ class CoinChanger
   def make_change(amount)
     coins = []
 
+    if amount >= 5
+      coins << "n"
+      amount -= 5
+    end
+
     while amount >= 1
       coins << "p"
       amount -= 1
